b1982390975b4c9fe1113d31ce504e737506c855
suchethan <chethan.reddy2020@gmail.com>
2023-02-01 22:12:42 +0530

    updating

--- counter/1.js
+++ counter/1.js
@@ -1,24 +1,42 @@
 let count =0 ;
 
-const decrease = document.getElementsByClassName('decrease');
-const increase = document.querySelector('.increase');
-const reset = document.querySelector('.reset');
+// const decrease = document.getElementsByClassName('decrease');
+// const increase = document.querySelector('.increase');
+// const reset = document.querySelector('.reset');
 
 const span = document.getElementById("value")
 
 
-//returns a html collection list 
-decrease[0].addEventListener("click", function(e) {
-    count--;
-    span.textContent = count;
-})
+// //returns a html collection list 
+// decrease[0].addEventListener("click", function(e) {
+//     count--;
+//     span.textContent = count;
+// })
 
-increase.addEventListener("click", function() {
-    count++;
-    span.textContent = count;
-})
+// increase.addEventListener("click", function() {
+//     count++;
+//     span.textContent = count;
+// })
 
-reset.addEventListener("click", function() {
-    count = 0;
-    span.textContent = count;
+// reset.addEventListener("click", function() {
+//     count = 0;
+//     span.textContent = count;
+// })
+
+// => further efficent code
+
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+        const value = e.currentTarget.classList; 
+        if(value.contains('decrease')) {
+            count--;
+        } else if(value.contains('increase')) {
+            count++;
+        } else{
+            count =0;
+        }
+        span.textContent = count;
+    })
 })
